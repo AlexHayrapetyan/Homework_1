b6e2c5a42f89a341d5a42772bf37954f0ebe5dd3
User <User@HP255G6>
2024-01-18 19:50:14 +0300

    Add project files.

---
+++ learning/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var temp = -18;
+
+            //if(temp > 0)
+            //{
+            //    Console.WriteLine("normal weather");
+            //}
+            //else if(temp < 0 || temp == 0)
+            //{
+            //    Console.WriteLine("freezing weather");
+            //}
+
+
+            //var age = 15;
+
+            //if(age < 13)
+            //{
+            //    Console.WriteLine("child");
+            //}
+            //else if(age >= 13 && age <= 19)
+            //{
+            //    Console.WriteLine("teenager");
+            //}
+            //else if(age > 19)
+            //{
+            //    Console.WriteLine("adult");
+            //}
+
+            //int[] numbers = { 1, 4, 23, 0, 32 };
+
+            //Console.WriteLine(numbers.Min());
+
+            //Console.WriteLine(numbers.Max());
+
+            //int[] numbers = { 1, 4, 23, 0, 32 };
+
+            //for (int i = numbers.Length - 1; i > 0; i--) 
+            //{
+            //   Console.WriteLine(numbers[i]);
+            //}
+            //Console.WriteLine(numbers[0]);
+
+            //int[] numbers = { 1, 4, 23, 0, 32 };
+
+            //Array.Sort(numbers);
+            //Console.WriteLine(numbers[numbers.Length - 2]);
+
+            //int[] arr1 = { 1, 2, 3 };
+            //int[] arr2 = { 4, 5, 6 };
+            //int[] sum_arr = {};
+            //for(int i = 0; i < arr1.Length; i++)
+            //{
+            //   sum_arr[i] = arr1[i] + arr2[i];
+            //    Console.WriteLine(sum_arr);
+            //}
+        }
+    }
+}
